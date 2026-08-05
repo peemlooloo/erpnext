42ae04d8599959e2543ac1e3c41f4a1d5bc2d32d
peemlooloo <p.peerayu@loolootech.com>
2026-08-05 10:45:31 +0700

    Fix sales team percentage validation rounding issue

--- erpnext/selling/doctype/customer/customer.py
+++ erpnext/selling/doctype/customer/customer.py
@@ -199,7 +199,7 @@ class Customer(TransactionBase):
 				self.loyalty_program_tier = customer.loyalty_program_tier
 
 		if self.sales_team:
-			if sum(member.allocated_percentage or 0 for member in self.sales_team) != 100:
+			if round(sum(member.allocated_percentage or 0 for member in self.sales_team), 2) != 100:
 				frappe.throw(_("Total contribution percentage should be equal to 100"))
 
 	@frappe.whitelist(methods=["POST"])
